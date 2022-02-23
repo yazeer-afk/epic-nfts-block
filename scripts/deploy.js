f1a0b5ab6ffe1671ef50cdc295ae9d1070ac9fd7
yazeer-afk <int.yazeer@gmail.com>
2022-02-23 11:21:06 +0530

    Hardhart configuration

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory("EpicNFT")
+    const nftContract = await nftContractFactory.deploy()
+    nftContract.deployed()
+
+    console.log("NFT Contract deployed to: ", nftContract.address)
+    
+    let txn = await nftContract.makeNFT()
+    await txn.wait()
+}
+
+const bootstrap = async () => {
+
+    try {
+        await main()
+        process.exit(0)
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+}
+
+bootstrap()
